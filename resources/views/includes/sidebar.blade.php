68ca673b4e0812f3a3b887b34eda56dcebf4be20
Abd Asis <34549079+abdasis@users.noreply.github.com>
2019-12-21 08:09:18 +0700

    update judul header

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -11,7 +11,7 @@
 
             <!-- Logo -->
             <div class="content-header-item">
-                <a class="link-effect font-w700" href="index.html">
+                <a class="link-effect font-w700" href="#">
                     <span class="font-size-xl text-dual-primary-dark">SKCK </span><span class="font-size-xl text-primary">PRIORITY</span>
                 </a>
             </div>
